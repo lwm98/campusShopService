b7a03928361786a7563b7c94d326a31a69d005d3
Jiesong.Zhang <javajies@163.com>
2019-11-12 01:04:36 +0800

    框架

---
+++ src/main/java/com/cigt/CigtApplication.java
@@ -0,0 +1,13 @@
+package com.cigt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CigtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CigtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cigt/CigtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cigt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CigtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
